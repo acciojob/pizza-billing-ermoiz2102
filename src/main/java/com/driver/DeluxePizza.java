901d84f5d98ae0e3e8df88391014469f895470c8
Moiz Khan <er.moiz2102@gmail.com>
2023-11-25 17:50:36 +0530

    code change

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,7 +3,23 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
-        super(isVeg);
-        // your code goes here
+       super(isVeg);
+       super.addExtraCheese();
+       super.addExtraToppings();
+    }
+
+    @Override
+    public void addTakeaway() {
+        super.addTakeaway();
+    }
+
+    @Override
+    public int getPrice() {
+        return super.getPrice();
+    }
+
+    @Override
+    public String getBill() {
+        return super.getBill();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,80 @@
 package com.driver;
 
 public class Pizza {
+    private final int veg=300;
+    private final int nonveg=400;
+    private final int topppr=70;
+    private final int nontopppr=120;
+    private final int cheesepr=80;
+    private final int takeaw=20;
 
     private int price;
-    private Boolean isVeg;
+    private boolean isVeg;
     private String bill;
+    private int topadd;
 
-    public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
+    private boolean take;
+    private boolean topp;
+    private boolean cheese;
+    public Pizza(boolean isVeg){
+        this.isVeg=isVeg;
+        if(isVeg){
+            price+=veg;
+
+        }
+        else
+            price+=nonveg;
     }
-
-    public int getPrice(){
-        return this.price;
-    }
-
     public void addExtraCheese(){
-        // your code goes here
+        if(!cheese){
+            cheese=true;
+            price+=cheesepr;
+        }
     }
-
     public void addExtraToppings(){
-        // your code goes here
-    }
+        if(!topp){
+            topp=true;
+            if(isVeg){
+                price+=topppr;
+                topadd=topppr;
+            }
 
+            else{
+                price+=nontopppr;
+                topadd=nontopppr;
+            }
+
+        }
+    }
     public void addTakeaway(){
-        // your code goes here
+        if(!take){
+            take=true;
+            price+=takeaw;
+        }
+
     }
 
-    public String getBill(){
-        // your code goes here
-        return this.bill;
+    public int getPrice() {
+        return price;
+    }
+
+    public String getBill() {
+        if(price>0){
+            if(isVeg)
+                bill="Base Price Of The Pizza: "+String.valueOf(veg)+"\n";
+            else
+                bill="Base Price Of The Pizza: "+String.valueOf(nonveg)+"\n";
+            if(cheese)
+                bill+="Extra Cheese Added: "+String.valueOf(cheesepr)+"\n";
+            if(topp){
+                bill+="Extra Toppings Added: "+String.valueOf(topadd)+"\n";
+            }
+            if(take)
+                bill+="Paperbag Added: "+String.valueOf(takeaw)+"\n";
+
+        }
+        bill+="Total Price: "+String.valueOf(price);
+
+        return bill;
     }
 }
